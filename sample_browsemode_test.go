5008c8f94e24e465bb5bf264ece893881aa447e5
Dmitriy <dmitrijsazonov@Dmitrijs-MacBook-Pro.local>
2020-05-07 19:18:31 +0300

    feat: add test for string body

--- sample_browsemode_test.go
+++ sample_browsemode_test.go
@@ -64,6 +64,155 @@ func TestBrowseAll(t *testing.T) {
 	clearQueue(cf, queue, countOfMessages)
 }
 
+func TestBrowseAllStringBody(t *testing.T) {
+	// Loads CF parameters from connection_info.json and apiKey.json in the Downloads directory
+	cf, cfErr := mqjms.CreateConnectionFactoryFromDefaultJSONFiles()
+	assert.Nil(t, cfErr)
+
+	cf.BrowseMode = true
+
+	// Creates a connection to the queue manager, using defer to close it automatically
+	// at the end of the function (if it was created successfully)
+	context, ctxErr := cf.CreateContext()
+
+	assert.Nil(t, ctxErr)
+	if context != nil {
+		defer context.Close()
+	}
+
+	// Equivalent to a JNDI lookup or other declarative definition
+	queue := context.CreateQueue("DEV.QUEUE.1")
+
+	// Set up the consumer ready to receive messages.
+	consumer, conErr := context.CreateConsumer(queue)
+	assert.Nil(t, conErr)
+	if consumer != nil {
+		defer consumer.Close()
+	}
+
+	// There is no messages in queue
+	testMsgList, err := consumer.BrowseAllStringBodyNoWait()
+	assert.Nil(t, testMsgList)
+	assert.Nil(t, err)
+
+	// Send a list of messages.
+	countOfMessages := 10
+	producer := context.CreateProducer()
+	for i := 0; i < countOfMessages; i++ {
+		bodyMsg := fmt.Sprintf("MyMessage_%d", i)
+		err := producer.SendString(queue, bodyMsg)
+		assert.Nil(t, err)
+	}
+
+	// Try to browse messages for first time
+	testMsgList, err2 := consumer.BrowseAllStringBodyNoWait()
+	assert.NotNil(t, testMsgList)
+	assert.Nil(t, err2)
+	assert.Equal(t, countOfMessages, len(testMsgList))
+
+	for i := 0; i < countOfMessages; i++ {
+		bodyMsg := fmt.Sprintf("MyMessage_%d", i)
+		assert.Equal(t, bodyMsg, *testMsgList[i])
+	}
+
+	// Try to browse second time
+	testMsgList, err2 = consumer.BrowseAllStringBodyNoWait()
+	assert.NotNil(t, testMsgList)
+	assert.Nil(t, err2)
+	assert.Equal(t, countOfMessages, len(testMsgList))
+
+	for i := 0; i < countOfMessages; i++ {
+		bodyMsg := fmt.Sprintf("MyMessage_%d", i)
+		assert.Equal(t, bodyMsg, *testMsgList[i])
+	}
+
+	// Clear queue
+	clearQueue(cf, queue, countOfMessages)
+}
+
+func TestBrowseAllStringBodyWait(t *testing.T) {
+	// Loads CF parameters from connection_info.json and apiKey.json in the Downloads directory
+	cf, cfErr := mqjms.CreateConnectionFactoryFromDefaultJSONFiles()
+	assert.Nil(t, cfErr)
+
+	cf.BrowseMode = true
+
+	// Creates a connection to the queue manager, using defer to close it automatically
+	// at the end of the function (if it was created successfully)
+	context, ctxErr := cf.CreateContext()
+
+	assert.Nil(t, ctxErr)
+	if context != nil {
+		defer context.Close()
+	}
+
+	// Equivalent to a JNDI lookup or other declarative definition
+	queue := context.CreateQueue("DEV.QUEUE.1")
+
+	// Set up the consumer ready to receive messages.
+	consumer, conErr := context.CreateConsumer(queue)
+	assert.Nil(t, conErr)
+	if consumer != nil {
+		defer consumer.Close()
+	}
+
+	waitTime := int32(500)
+
+	// No messages in queue
+	startTime := currentTimeMillis()
+	testMsgList, err := consumer.BrowseAllStringBody(waitTime)
+	endTime := currentTimeMillis()
+	assert.Nil(t, testMsgList)
+	assert.Nil(t, err)
+
+	// Within a reasonable margin of the expected wait time.
+	assert.True(t, (endTime-startTime-int64(waitTime)) > -100)
+
+	// Send a list of messages.
+	countOfMessages := 10
+	producer := context.CreateProducer()
+	for i := 0; i < countOfMessages; i++ {
+		bodyMsg := fmt.Sprintf("MyMessage_%d", i)
+		err := producer.SendString(queue, bodyMsg)
+		assert.Nil(t, err)
+	}
+
+	// Try to browse messages
+	startTime = currentTimeMillis()
+	testMsgList, err = consumer.BrowseAllStringBody(waitTime)
+	endTime = currentTimeMillis()
+	assert.NotNil(t, testMsgList)
+	assert.Nil(t, err)
+	assert.Equal(t, countOfMessages, len(testMsgList))
+
+	// Within a reasonable margin of the expected wait time.
+	assert.True(t, (endTime-startTime) < int64(waitTime*int32(countOfMessages)))
+
+	for i := 0; i < countOfMessages; i++ {
+		bodyMsg := fmt.Sprintf("MyMessage_%d", i)
+		assert.Equal(t, bodyMsg, *testMsgList[i])
+	}
+
+	// Try to browse message for the second time
+	startTime = currentTimeMillis()
+	testMsgList, err = consumer.BrowseAllStringBody(waitTime)
+	endTime = currentTimeMillis()
+	assert.NotNil(t, testMsgList)
+	assert.Nil(t, err)
+	assert.Equal(t, countOfMessages, len(testMsgList))
+
+	// Within a reasonable margin of the expected wait time.
+	assert.True(t, (endTime-startTime) < int64(waitTime*int32(countOfMessages)))
+
+	for i := 0; i < countOfMessages; i++ {
+		bodyMsg := fmt.Sprintf("MyMessage_%d", i)
+		assert.Equal(t, bodyMsg, *testMsgList[i])
+	}
+
+	// Clear queue
+	clearQueue(cf, queue, countOfMessages)
+}
+
 func TestBrowseAllWait(t *testing.T) {
 	// Loads CF parameters from connection_info.json and apiKey.json in the Downloads directory
 	cf, cfErr := mqjms.CreateConnectionFactoryFromDefaultJSONFiles()
